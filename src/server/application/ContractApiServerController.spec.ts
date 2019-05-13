afe4fbfa6d1733fbc44e4df2d26b80f5dd71942f
rhyslbw <rhyslbw@gmail.com>
2019-05-14 00:25:05 +1000

    Refactor ContractApiServerController to manually setup express server in order to gracefully handle EADDRINUSE errors. Also updates deps for compatibility

--- src/server/application/ContractApiServerController.spec.ts
+++ src/server/application/ContractApiServerController.spec.ts
@@ -9,8 +9,105 @@ import { InMemoryRepository } from '../infrastructure/repositories'
 import { StubEngineClient } from '../infrastructure/engine_clients'
 import { testContract } from './test'
 import { PortAllocation } from '../core'
+import { AllPortsAllocated } from '../core/errors'
+import { Config as PortManagerConfig } from './PortManager'
+import { RogueService } from './test/Helpers'
 
 use(chaiAsPromised)
+
+describe('ContractApiServerController', () => {
+  describe('Full TCP port range available', () => {
+    let controller: ReturnType<typeof ContractApiServerController>
+    let engineClient: ReturnType<typeof StubEngineClient>
+    let portAllocationRepository: Repository<PortAllocation>
+    let schema: IExecutableSchemaDefinition
+    beforeEach(() => {
+      portAllocationRepository = InMemoryRepository<PortAllocation>()
+      engineClient = StubEngineClient()
+      schema = testContract.graphQLSchema(engineClient)
+      controller = ContractApiServerController(
+        PortManager({
+          repository: portAllocationRepository,
+          range: { lower: 8082, upper: 8084 }
+        })
+      )
+    })
+    afterEach(async () => {
+      await controller.closeAllServers()
+    })
+
+    describe('deploy', () => {
+      it("Deploys the contract's GraphQl server using the next port in the range if available", async () => {
+        const deploy = await controller.deploy(testContract.address, schema)
+        expect(deploy).to.be.true
+        expect((await checkServer(8082)).statusText).to.eq('OK')
+      })
+      it('Returns true if the contract is already deployed', async () => {
+        expect(await controller.deploy(testContract.address, schema)).to.be.true
+        const deploy = await controller.deploy(testContract.address, schema)
+        expect(deploy).to.be.true
+        expect((await checkServer(8082)).statusText).to.eq('OK')
+      })
+    })
+
+    describe('tearDown', () => {
+      it('closes the server for the specified contract', async () => {
+        await controller.deploy(testContract.address, schema)
+        expect((await checkServer(8082)).statusText).to.eq('OK')
+        const tearDown = await controller.tearDown(testContract.address)
+        expect(tearDown).to.be.true
+        await expect(checkServer(8082)).to.be.rejected
+      })
+      it('returns false if contract not deployed', async () => {
+        const tearDown = await controller.tearDown(testContract.address)
+        expect(tearDown).to.be.false
+      })
+    })
+
+    describe('closeAllServers', () => {
+      beforeEach(async () => {
+        await controller.deploy(testContract.address, schema)
+        await controller.deploy('another_contract', schema)
+        expect((await checkServer(8082)).statusText).to.eq('OK')
+        expect((await checkServer(8083)).statusText).to.eq('OK')
+      })
+      it('closes all servers ', async () => {
+        await controller.closeAllServers()
+        await expect(checkServer(8082)).to.be.rejected
+        await expect(checkServer(8083)).to.be.rejected
+      })
+    })
+  })
+  describe('Rogue services in the allocated TCP port range', () => {
+    let controller: ReturnType<typeof ContractApiServerController>
+    let engineClient: ReturnType<typeof StubEngineClient>
+    let portAllocationRepository: Repository<PortAllocation>
+    let schema: IExecutableSchemaDefinition
+    beforeEach(() => {
+      portAllocationRepository = InMemoryRepository<PortAllocation>()
+      engineClient = StubEngineClient()
+      schema = testContract.graphQLSchema(engineClient)
+      controller = ContractApiServerController(
+        CollidablePortManager({
+          repository: portAllocationRepository,
+          range: { lower: 8082, upper: 8084 }
+        })
+      )
+    })
+    afterEach(async () => {
+      await controller.closeAllServers()
+    })
+
+    it('Requests a new port allocation if a collision occurs when starting the server', async () => {
+      const rogueService = RogueService()
+      await rogueService.listen(8082)
+      const deploy = await controller.deploy(testContract.address, schema)
+      expect(deploy).to.be.true
+      expect((await checkServer(8083)).statusText).to.eq('OK')
+      rogueService.close()
+    })
+  })
+})
 
 function checkServer (port: number) {
   return axios({
@@ -20,65 +117,27 @@ function checkServer (port: number) {
   })
 }
 
-describe('ContractApiServerController', () => {
-  let controller: ReturnType<typeof ContractApiServerController>
-  let engineClient: ReturnType<typeof StubEngineClient>
-  let portAllocationRepository: Repository<PortAllocation>
-  let schema: IExecutableSchemaDefinition
-  beforeEach(() => {
-    portAllocationRepository = InMemoryRepository<PortAllocation>()
-    engineClient = StubEngineClient()
-    schema = testContract.graphQLSchema(engineClient)
-    controller = ContractApiServerController(
-      PortManager({
-        repository: portAllocationRepository,
-        range: { lower: 8082, upper: 8084 }
-      })
-    )
-  })
-  afterEach(async () => {
-    await controller.closeAllServers()
-  })
-
-  describe('deploy', () => {
-    it("Deploys the contract's GraphQl server using the next port in the range if available", async () => {
-      const deploy = await controller.deploy(testContract.address, schema)
-      expect(deploy).to.be.true
-      expect((await checkServer(8082)).statusText).to.eq('OK')
-    })
-    it('Returns true if the contract is already deployed', async () => {
-      expect(await controller.deploy(testContract.address, schema)).to.be.true
-      const deploy = await controller.deploy(testContract.address, schema)
-      expect(deploy).to.be.true
-      expect((await checkServer(8082)).statusText).to.eq('OK')
-    })
-  })
-
-  describe('tearDown', () => {
-    it('closes the server for the specified contract', async () => {
-      await controller.deploy(testContract.address, schema)
-      expect((await checkServer(8082)).statusText).to.eq('OK')
-      const tearDown = await controller.tearDown(testContract.address)
-      expect(tearDown).to.be.true
-      await expect(checkServer(8082)).to.be.rejected
-    })
-    it('returns false if contract not deployed', async () => {
-      const tearDown = await controller.tearDown(testContract.address)
-      expect(tearDown).to.be.false
-    })
-  })
-
-  describe('closeAllServers', () => {
-    beforeEach(async () => {
-      await controller.deploy(testContract.address, schema)
-      await controller.deploy('another_contract', schema)
-      expect((await checkServer(8082)).statusText).to.eq('OK')
-      expect((await checkServer(8083)).statusText).to.eq('OK')
-    })
-    it('closes all servers ', async () => {
-      await controller.closeAllServers()
-      await expect(checkServer(8082)).to.be.rejected
-      await expect(checkServer(8083)).to.be.rejected
-    })
-  })
-})
+function CollidablePortManager ({ repository, range }: PortManagerConfig) {
+  const startingPoolQty = range.upper - (range.lower - 1)
+  return {
+    isAvailable: async (): Promise<boolean> => {
+      return true
+    },
+    getAvailablePort: async (): Promise<PortAllocation> => {
+      const size = await repository.size()
+      if (size === startingPoolQty) throw new AllPortsAllocated(range)
+      const portNumber = size === 0
+        ? range.lower
+        : (await repository.getLast()).portNumber + 1
+      const portAllocation = {
+        id: portNumber.toString(),
+        portNumber
+      }
+      await repository.add(portAllocation)
+      return portAllocation
+    },
+    releasePort: async (): Promise<boolean> => {
+      return true
+    }
+  }
+}
--- src/server/application/ContractApiServerController.ts
+++ src/server/application/ContractApiServerController.ts
@@ -1,6 +1,8 @@
-import http from 'http'
+import * as http from 'http'
+import * as express from 'express'
 import { AddressInfo } from 'net'
-import { ApolloServer, makeExecutableSchema, IExecutableSchemaDefinition } from 'apollo-server'
+import { ApolloServer } from 'apollo-server-express'
+import { makeExecutableSchema, IExecutableSchemaDefinition } from 'apollo-server'
 import { PortManager } from '.'
 import { Contract } from '../core'
 
@@ -24,12 +26,21 @@ export function ContractApiServerController (portManager: ReturnType<typeof Port
     async deploy (contractAddress: Contract['address'], graphQlSchema: IExecutableSchemaDefinition): Promise<boolean> {
       if (servers.has(contractAddress)) return true
       const allocation = await portManager.getAvailablePort()
-      const serverInfo = await new ApolloServer({
+      const app = express()
+      const apolloServer = new ApolloServer({
         schema: makeExecutableSchema(graphQlSchema),
         introspection: true
-      }).listen({ port: allocation.portNumber })
-      servers.set(contractAddress, serverInfo.server)
-      return true
+      })
+      apolloServer.applyMiddleware({ app })
+      try {
+        const server = await listen(app, allocation.portNumber)
+        servers.set(contractAddress, server)
+        return true
+      } catch (error) {
+        if (error.code === 'EADDRINUSE') {
+          return this.deploy(contractAddress, graphQlSchema)
+        }
+      }
     },
     async tearDown (contractAddress: Contract['address']): Promise<boolean> {
       const serverInfo = servers.get(contractAddress)
@@ -38,10 +49,18 @@ export function ContractApiServerController (portManager: ReturnType<typeof Port
       return true
     },
     async closeAllServers () {
-      await Promise.all([...servers.entries()].map(([contractAddress, serverInfo]) => {
-        return shutdownServer(contractAddress, serverInfo)
+      await Promise.all([...servers.entries()].map(([contractAddress, server]) => {
+        return shutdownServer(contractAddress, server)
       }))
       return true
     }
   }
+}
+
+async function listen (app: ReturnType<typeof express>, port: number): Promise<http.Server> {
+  return new Promise((resolve, reject) => {
+    const server: http.Server = app.listen({ port })
+      .on('listening', () => resolve(server))
+      .on('error', (error) => reject(error))
+  })
 }
