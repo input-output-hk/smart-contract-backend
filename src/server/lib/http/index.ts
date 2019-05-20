bffcae4f9999ca6865f67dcee0e26a4157697777
rhyslbw <rhyslbw@gmail.com>
2019-05-20 13:57:56 +1000

    Simplify http lib functions

--- src/server/lib/http/index.ts
+++ src/server/lib/http/index.ts
@@ -3,15 +3,15 @@ import * as http from 'http'
 export async function listen (server: http.Server, port: number): Promise<http.Server> {
   return new Promise((resolve, reject) => {
     server.listen({ port })
-      .on('listening', () => resolve(server))
-      .on('error', (error) => reject(error))
+      .on('listening', resolve)
+      .on('error', reject)
   })
 }
 
 export async function close (server: http.Server): Promise<void> {
   return new Promise((resolve, reject) => {
     server.close()
-      .on('close', () => resolve())
-      .on('error', (error) => reject(error))
+      .on('close', resolve)
+      .on('error', reject)
   })
 }
