254cf53fda0274d8334ed3481bdd58e9497831ef
rhyslbw <rhyslbw@gmail.com>
2019-05-28 13:42:55 +1000

    Update custom error classes

--- src/server/core/errors/BundleNotFound.ts
+++ src/server/core/errors/BundleNotFound.ts
@@ -1,9 +1,8 @@
 import { CustomError } from 'ts-custom-error'
 
 export class BundleNotFound extends CustomError {
-  constructor (location: string) {
+  public constructor (location: string) {
     super()
     this.message = `Bundle not found at ${location}`
-    this.name = 'BundleNotFound'
   }
 }
