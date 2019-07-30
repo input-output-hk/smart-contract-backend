e47f4d3497b6b0d211454746abf3f8e2b5c7296f
Sam Jeston <sam.jeston@iohk.io>
2019-07-31 09:28:16 +1000

    Add dependency validation to bundling job

--- bundle/bundle_and_store.js
+++ bundle/bundle_and_store.js
@@ -2,9 +2,9 @@ const os = require('os')
 const platform = os.platform()
 const { exec, execFile } = require('child_process')
 const { join } = require('path')
-const fs = require('fs')
 const archiver = require('archiver')
 const AWS = require('aws-sdk')
+const fs = require('fs-extra')
 
 const S3_BUCKET = 'smart-contract-backend-builds'
 
@@ -20,20 +20,20 @@ function commitHash() {
 function createBundle() {
   return new Promise((resolve, reject) => {
     if (platform === 'win32') {
-      execFile(join(process.cwd(), 'bundle', 'windows.bat'), function (err) {
+      execFile(join(process.cwd(), 'bundle', 'windows.bat'), function (err, stdout) {
         if (err) return reject(err)
-        resolve()
+        resolve(stdout)
       })
     } else {
-      execFile(join(process.cwd(), 'bundle', 'unix.sh'), function (err) {
+      execFile(join(process.cwd(), 'bundle', 'unix.sh'), function (err, stdout) {
         if (err) return reject(err)
-        resolve()
+        resolve(stdout)
       })
     }
   })
 }
 
-function createZip(bundlePath, zipPath, exeName) {
+function createZip(bundlePath, zipPath, exeName, buildDeps) {
   return new Promise((resolve, reject) => {
     let output = fs.createWriteStream(zipPath)
     var archive = archiver('zip')
@@ -47,7 +47,11 @@ function createZip(bundlePath, zipPath, exeName) {
     archive.pipe(output)
 
     archive.file(join(bundlePath, exeName), { name: exeName })
-    archive.directory(join(bundlePath, 'puppeteer'), 'puppeteer')
+
+    buildDeps.forEach(dep => {
+      archive.directory(join(bundlePath, dep), dep)
+    })
+
     archive.finalize()
   })
 }
@@ -88,19 +92,43 @@ async function uploadToS3(s3Path, zipPath) {
   await s3.putObject(createParams).promise()
 }
 
+async function validateDependencies(buildOutput, bundlePath) {
+  const testExpression = RegExp('path-to-executable*', 'g')
+  const pkgDeclaredBuildDeps = buildOutput
+    .split('\n')
+    .filter(line => testExpression.test(line))
+    .map(filteredLine => filteredLine.split('path-to-executable/')[1])
+
+  const validatePaths = await Promise.all(pkgDeclaredBuildDeps.map(dep => {
+    return fs.pathExists(join(bundlePath, dep))
+  }))
+
+  if (new Set(validatePaths).has(false)) {
+    throw new Error(`Missing dependencies. Ensure the build copies ${pkgDeclaredBuildDeps} from appropriate locations`)
+  }
+
+  return pkgDeclaredBuildDeps
+}
+
 async function main() {
   try {
     const hash = await commitHash()
     console.log(`Creating Build for commit: ${hash}`)
-    await createBundle()
+    const buildOutput = await createBundle()
 
     const { bundlePath, exeName, s3Path } = determineOsInfo(hash)
+
+    // We use the output of the pkg and validate that all
+    // of the native addons are included at the expected file
+    // location before adding them to the zip
+    const buildDeps = await validateDependencies(buildOutput, bundlePath)
+
     const zipPath = `${bundlePath}.zip`
     console.log(`Creating zip at ${zipPath}`)
-    await createZip(bundlePath, zipPath, exeName)
+    await createZip(bundlePath, zipPath, exeName, buildDeps)
 
     console.log('Uploading to S3...')
-    await uploadToS3(s3Path, zipPath)
+    // await uploadToS3(s3Path, zipPath)
   } catch (e) {
     console.log('An error occurred while creating the bundle')
     console.log(e.message)
