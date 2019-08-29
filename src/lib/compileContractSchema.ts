788c919b4e0f5e7d3adba5a11fb46adde80b92d7
Sam Jeston <sam.jeston@iohk.io>
2019-08-29 11:08:19 +1000

    WIP - Dynamic call to tsc

--- src/lib/compileContractSchema.ts
+++ src/lib/compileContractSchema.ts
@@ -2,35 +2,54 @@ import * as fs from 'fs-extra'
 import { exec } from 'child_process'
 const uuidv4 = require('uuid/v4')
 
-// The uncompiled schema relies the following imports but they are
+// The uncompiled schema relies on the following imports but they are
 // not declared from the Plutus generator.
 
 // This function is called when dynamically loading straight off of
 // the Plutus contract, or we creating a bundle for distribution
 export async function compileContractSchema (uncompiledSchema: string) {
   const tempfileAllocator = uuidv4()
-
   const tscFileName = `${tempfileAllocator}.ts`
   const outputFileName = `${tempfileAllocator}.js`
   const tempTscFilePath = `${__dirname}/${tscFileName}`
   const tempOutputFilePath = `${__dirname}/${outputFileName}`
 
-  const iotsPrefix = `import * as t from 'io-ts'`
-  const createEndpointPrefix = `import { createEndpoint } from ${__dirname}/createEndpoint`
+  try {
+    const iotsPrefix = `import * as t from 'io-ts'`
+    const createEndpointPrefix = `import { createEndpoint } from '${__dirname}/createEndpoint'`
 
-  const uncompiledSchemaWithImports = `
-    ${iotsPrefix}
-    ${createEndpointPrefix}
-    ${uncompiledSchema}
-  `
+    const uncompiledSchemaWithImports = `
+      ${iotsPrefix}
+      ${createEndpointPrefix}
+      ${uncompiledSchema}
+    `
 
-  await fs.writeFile(tempTscFilePath, uncompiledSchemaWithImports)
-  await exec(`npx tsc ${tempTscFilePath} --out ${tempOutputFilePath}`)
+    await fs.writeFile(tempTscFilePath, uncompiledSchemaWithImports)
+    await tscExecutionHandler(tempTscFilePath, tempOutputFilePath)
 
-  const compiledSchema = (await fs.readFile(tempOutputFilePath)).toString()
+    const compiledSchema = (await fs.readFile(tempOutputFilePath)).toString()
+    return compiledSchema
+  } catch (e) {
+    throw e
+  } finally {
+    await fs.remove(tempTscFilePath)
+    await fs.remove(tempOutputFilePath)
+  }
+}
 
-  await fs.remove(tempTscFilePath)
-  await fs.remove(tempOutputFilePath)
+function tscExecutionHandler(tscFilePath: string, outputFilePath: string) {
+  return new Promise(async (resolve, reject) => {
+    exec(`cd ../.. && npx tsc ${tscFilePath} --module amd --out ${outputFilePath}`, (err, stdout, stderr) => {
+      if (err) {
+        reject(new Error('tsc error'))
+      }
 
-  return compiledSchema
+      if (stderr) {
+        reject(new Error('tsc error'))
+      }
+
+      console.log(stdout)
+      resolve()
+    })
+  })
 }
